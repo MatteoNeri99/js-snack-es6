a95a5fb4ac608afff20c3ebf0d43b95c5e438cc1
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-23 16:48:29 +0200

    aggiunto array di oggetti

--- script.js
+++ script.js
@@ -23,5 +23,75 @@
 // console.log(invitatiAndOrdine);
 
 
+const studenti=[
 
+
+    {
+
+        id:213,
+
+        name:"Giuseppina della Rovere",
+
+        grades:78,
+
+    },
+
+    {
+
+        id:110,
+
+        name:"Paola Cortellessa ",
+
+        grades:96,
+
+    },
+
+    {
+
+        id:250,
+
+        name:" Andrea Mantegna ",
+
+        grades:48,
+
+    },
+
+    {
+
+        id:145,
+
+        name:"Gaia Borromini",
+
+        grades:74,
+
+    },
+    {
+
+        id:196,
+
+        name:"Luigi Grimaldello",
+
+        grades:68,
+
+    },
+    {
+
+        id:102,
+
+        name:"Piero della Francesca ",
+
+        grades:50,
+
+    },
+    {
+
+        id:120,
+
+        name:"Francesca da Polenta",
+
+        grades:84,
+
+    },
+
+]
 
