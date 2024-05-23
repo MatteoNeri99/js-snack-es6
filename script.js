795a1d49ee1e55a6d76ba3447555907ba78449a6
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-23 14:38:21 +0200

    creato ciclo for che inserisce i valori del primo array ad un secondo array, li trasforma in oggetti e gli aggiunge altre due chiavi con rispettivo valore

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 const invitati=[ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
 
+
+const invitatiAndOrdine=[];
+
+invitati.map((element) => {
+
+    return element + {}
+
+    
+
+});
+
+
+for (let index=0; index<invitati.length; index++){
+
+    invitatiAndOrdine.push({"guestName" : invitati[index] , "tabelename" : "Tavolo Vip" , "place" : index } )
+    
+    
+    console.log(invitatiAndOrdine[index].guestName);
+}
+
+// console.log(invitatiAndOrdine);
+
+
+
+
